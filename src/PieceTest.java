a7e631630d95b0dade3b74bce8d5caadedc725cd
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-04-22 14:21:02 -0500

    simple moves test finished

--- src/PieceTest.java
+++ src/PieceTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class PieceTest {
     private void testPiece(AbstractPiece p, String symbol) {
         Assert.assertEquals(symbol, p.getSymbol());
@@ -12,4 +14,27 @@ public class PieceTest {
 //        AbstractPiece p1 = new AbstractPiece(s1);
 //        testPiece(p1, s1);
 //    }
+    @Test
+    public void testSimpleMoves() {
+        CheckersGame cg = new CheckersGame();
+        CheckersBoard c1 = new CheckersBoard(cg);
+        CheckersPiece p1 = (CheckersPiece) cg.getBoard().getGrid().get(2).get(1);
+        CheckersPiece p2 = (CheckersPiece) cg.getBoard().getGrid().get(0).get(7);
+        CheckersMoveStrategy cs = new CheckersMoveStrategy(cg.getP2());
+
+        Assert.assertFalse(p1.findSimpleMoves().isEmpty());
+
+        Assert.assertArrayEquals(new int[] {1,2,2,3},
+                p1.findSimpleMoves().get(0));
+        Assert.assertArrayEquals(new int[] {1,2,0,3},
+                p1.findSimpleMoves().get(1));
+        Assert.assertTrue(p2.findSimpleMoves().isEmpty());
+
+        cg.getP2().takeMove(new int[] {4,5,3,4});
+        cg.getP2().takeMove(new int[] {3,4,2,3});
+        CheckersPiece p3 = (CheckersPiece) cg.getBoard().getGrid().get(2).get(1);
+
+        Assert.assertArrayEquals(new int[] {1,2,0,3},
+                p3.findSimpleMoves().get(0));
+    }
 }
